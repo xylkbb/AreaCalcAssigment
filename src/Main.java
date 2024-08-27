f6ee0af1470c704e97f40450b2d04a9ba248d3b9
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-27 13:44:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+
+    }
+
+    private static double area(double x, double y) {
+
+        if(x < 0){
+            return -1.0;
+        }
+
+        if(y < 0){
+            return -1.0;
+        }
+
+
+        return x * y;
+    }
+
+    private static double area(double radius) {
+        if(radius < 0){
+            return -1.0;
+        }
+
+        return Math.PI * radius * radius;
+
+    }
+
+}
